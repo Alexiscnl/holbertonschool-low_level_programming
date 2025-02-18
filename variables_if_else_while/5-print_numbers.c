7de34d95acb5ea1f13f85f016b90ec9a82e58196
Alexiscnl <alexis.cornillon@gmail.com>
2025-02-18 14:55:25 +0100

    compter_1_9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char number = '0';
+
+	while (0 <= '9')
+	{
+	putchar(number++);
+	}
+	putchar('\n');
+	return (0);
+}
