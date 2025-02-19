e8ab24e8c6696a02aaaa6a1b80bac720b5b86fab
Alexiscnl <alexis.cornillon@gmail.com>
2025-02-19 17:19:56 +0100

    return_last_digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ *@n: the caracter
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = n % 10;
+	if (last_digit < 0)
+	{
+		last_digit = -last_digit;
+	}
+	_putchar(0 + last_digit);
+	return (last_digit);
+}
