470012532667772d170e074c3e6cb2e66d49f429
Alexiscnl <alexis.cornillon@gmail.com>
2025-02-19 14:21:15 +0100

    change_description

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - print 10x l'alphabet 
+ * print_alphabet_x10 - print 10x l'alphabet
  *
  * Return: void
  */
@@ -8,10 +8,10 @@ void print_alphabet_x10(void)
 {
 	char lettre = 'a';
 	int ligne = 0;
-	
+
 	for (ligne = 0; ligne < 10; ligne++)
 	{
-		for (lettre = 'a'; lettre <= 'z';lettre++)
+		for (lettre = 'a'; lettre <= 'z'; lettre++)
 		{
 		_putchar(lettre);
 		}
