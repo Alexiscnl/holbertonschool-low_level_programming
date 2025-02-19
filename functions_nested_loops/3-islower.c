34e5ed26fbffaee45385c49e6abf15bdc9e4b329
Alexiscnl <alexis.cornillon@gmail.com>
2025-02-19 15:15:04 +0100

    return0_return1

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - print 1 is lowercase otherwise 0
+ * @c: the carracter
+ *
+ * Return: 1 other lowercase otherwise 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
