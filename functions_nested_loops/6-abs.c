d8879b58c59d387317b7bc37c1176504eb0cb90a
Alexiscnl <alexis.cornillon@gmail.com>
2025-02-19 16:58:35 +0100

    return_positiv_or_negativ

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - print number negativ or positiv
+ *@n: the caracter
+ *
+ * Return: positiv if n < 0, otherwise return negativ
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
