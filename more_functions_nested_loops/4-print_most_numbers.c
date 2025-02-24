01a6f4d36c72f3addff3c84db96804baa7f6aed7
Alexiscnl <alexis.cornillon@gmail.com>
2025-02-24 14:23:35 +0100

    change_putchar

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
+ * print_most_numbers - display numbers except 2 and 4
  *
- *
- *
+ * no Return
  */
 void print_most_numbers(void)
 {
@@ -10,12 +10,11 @@ void print_most_numbers(void)
 
 	for (; numbers < 10; numbers++)
 	{
-		_putchar(numbers + '0');
-	
-		if (numbers != 2 && numbers != 4)
+		if (numbers == 2 || numbers == 4)
 		{
 			numbers++;
 		}
+		_putchar(numbers + '0');
 	}
 	_putchar('\n');
 }
