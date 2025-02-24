3aa349cacf7007fff7b8e935f7c2b7b5ae633063
Alexiscnl <alexis.cornillon@gmail.com>
2025-02-24 10:52:34 +0100

    printA:1_if_uppercase_otherwise_printa:0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - dyplay 0 if lower otherwise dysplay 1
+ *@c: A: , a:
+ *
+ * Return: 0 if uppercase otherwise return 1
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
