9cf0ffce29e5286de7caa9c75b87691920f9e0fe
Alexiscnl <alexis.cornillon@gmail.com>
2025-02-25 16:08:28 +0100

    change_1task_and_finish_task5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - reverse string my school
+ *@s: pointer the string
+ *
+ * no Return
+ */
+void rev_string(char *s)
+{
+	int index = 0;
+	int index2 = 0;
+	int temp = 0;
+
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+
+	while (index2 <= index)
+	{
+		index--;
+		temp = s[index];
+		s[index] = s[index2];
+		s[index2] = temp;
+		index2++;
+	}
+}
