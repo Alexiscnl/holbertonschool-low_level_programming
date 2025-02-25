a6e65c20ebbe34427fb590593265b1a46e83efdb
Alexiscnl <alexis.cornillon@gmail.com>
2025-02-25 11:23:51 +0100

    change_a_and_b

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,6 +1,15 @@
 #include "main.h"
 /**
+ * swap_int - change variable
+ *@a: pointer 98
+ *@b: pointer 42
  *
- *
- *
+ * no Return
  */
+void swap_int(int *a, int *b)
+{
+	int p = *a;
+
+	*a = *b;
+	*b = p;
+}
