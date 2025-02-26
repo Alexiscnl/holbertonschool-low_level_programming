80b012c0edce1f87b9d54fe7b227a791d94b2285
Alexiscnl <alexis.cornillon@gmail.com>
2025-02-26 11:19:27 +0100

    change_code_delete_i

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,9 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
-
-	for (i = 0; i <= n; i++)
+	for (n = 0; n <= 3; n++)
 	{
-		printf("%d", a[i]);
-		if (i < n - 1)
-		{
-			printf(", ");
-		}
+		printf("%d, ", a[n]);
 	}
-	printf("\n");
+	printf("%d\n", a[n]);
 }
