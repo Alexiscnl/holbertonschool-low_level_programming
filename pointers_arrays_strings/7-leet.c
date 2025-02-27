67f720a1029a83b21c747b014e19c263f8b3d117
Alexiscnl <alexis.cornillon@gmail.com>
2025-02-28 00:49:07 +0100

    remplace_letter_for_number_with_1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *leet - converts a string
+ *@i: sentence
+ *
+ * Return: copy
+ */
+char *leet(char *i)
+{
+	char *copy = i;
+	char let[] = "aAeEoOtTlL";
+	char remp[] = "4433007711";
+	int a;
+
+	for (; *i != '\0'; i++)
+	{
+		for (a = 0; let[a] != '\0'; a++)
+		{
+			if (*i == let[a])
+			{
+				*i = remp[a];
+			}
+		}
+		i++;
+	}
+	return (copy);
+}
