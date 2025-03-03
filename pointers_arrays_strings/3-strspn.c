524c2b90105471720bdef36d3e1a26dfed322749
Alexiscnl <alexis.cornillon@gmail.com>
2025-03-03 11:22:53 +0100

    change_strchr

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * _strspn - Gets the length of a prefix substring.
  * @s: The string to be checked.
@@ -9,12 +8,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-int i;
-int counter = 0;
+int i, j;
+unsigned int counter = 0;
 
 for (i = 0; s[i] != '\0'; i++)
 {
-if (_strchr(accept, s[i]))
+int found = 0;
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+found = 1;
+break;
+}
+}
+if (found)
 {
 counter++;
 }
@@ -22,6 +30,7 @@ else
 {
 break;
 }
+
 }
 return (counter);
 }
