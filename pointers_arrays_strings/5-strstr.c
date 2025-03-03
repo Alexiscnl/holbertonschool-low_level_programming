a064430302eef60f40cc3f25003cba7aac88a1a5
Alexiscnl <alexis.cornillon@gmail.com>
2025-03-03 13:27:23 +0100

    sous_chaine

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -10,16 +10,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i, j;
-
-for (i = 0; haystack[i] != '\0'; i++)
+if (*needle == '\0')
 {
-for (j = 0; needle[j] != '\0'; j++)
-{
-if (haystack[i] == needle[j])
-{
-return (haystack + 7);
+return (haystack);
 }
+for (; *haystack != '\0'; haystack++)
+{
+char *n = needle;
+char *h = haystack;
+while (*h != '\0' && *n != '\0' && *h == *n)
+{
+h++;
+n++;
+}
+if (*n == '\0')
+{
+return (haystack);
 }
 }
 return (NULL);
