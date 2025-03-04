b75072ae79d822d65317a52370082c3237e8f1f1
Alexiscnl <alexis.cornillon@gmail.com>
2025-03-04 10:09:42 +0100

    first_essai_task1

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,16 @@
 char *_strchr(char *s, char c)
 {
 
-for (; *s != '\0'; s++)
-{
-if (*s == c)
-{
-return (s);
-}
-}
-if (c == '\0')
-{
-return (s);
-}
-return (NULL);
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
 }
