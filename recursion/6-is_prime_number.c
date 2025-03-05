0ff9e8e93acb9c4b5a78be8c90e5ce01348d4855
Alexiscnl <alexis.cornillon@gmail.com>
2025-03-05 10:32:47 +0100

    change_line_17_1_for_i

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,26 +1,37 @@
 #include "main.h"
 #include <math.h>
-#include <stdio.h>
 /**
- * is_prime_number - Recursive function to test divisibility
- * @n: The number to test
+ * is_two_function - Recursive helper function to check if n is divisible by i
+ * @n: The number to check
+ * @i: The current divisor to test
  *
  * Return: 1 if n is prime, 0 otherwise
+ *
+ */
+int is_two_function(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_two_function(n, i + 1));
+}
+/**
+ * is_prime_number - Main function to check if n is prime
+ * @n: The number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ *
  */
 int is_prime_number(int n)
 {
-	int i = 2;
-
 	if (n <= 1)
 	{
 		return (0);
 	}
-	for (; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (is_two_function(n, 2));
 }
