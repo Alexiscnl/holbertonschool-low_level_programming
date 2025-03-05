fa483293b1ecd9e47d8f8de13813ed7dc6080deb
Alexiscnl <alexis.cornillon@gmail.com>
2025-03-05 11:36:12 +0100

    print_number_string

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print number string
+ *@argc: The number of command line arguments.
+ *@argv: An array of strings representing the command line arguments.
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
