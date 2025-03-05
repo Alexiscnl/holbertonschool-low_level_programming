b3005e82d5079babac4f2e52a17d3f913575604c
Alexiscnl <alexis.cornillon@gmail.com>
2025-03-05 12:40:49 +0100

    multiplie_argv

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies argv1 and 2
+ *@argc: The number of command line arguments
+ *@argv: An array of strings representing the command line arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc != 3)
+	{
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+	return (0);
+}
