87a752a0c256c88447837d0d3c9d38b9b14cc2a5
Alexiscnl <alexis.cornillon@gmail.com>
2025-03-10 11:32:25 +0100

    first_malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - Creates an array of chars and initializes it with a specific char
+ * @size: The size of the array to be created
+ * @c: The character to initialize the array with
+ *
+ * Return: A pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *arr;
+	if (size == 0)
+	{
+		return NULL;
+	}
+	arr = malloc(size * sizeof(char));
+
+	for (; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
