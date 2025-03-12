f7413073af56a68094dede4c1f9001bea4c63817
Alexiscnl <alexis.cornillon@gmail.com>
2025-03-12 15:05:26 +0100

    change_incre_j-for_i

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,6 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * _lenght - Calculates the length of a string.
+ * @b: The string whose length is to be calculated.
+ *
+ * Return: The length of the string.
+ */
+int _lenght(char *b)
+{
+	int a = 0;
+
+	while (*b++)
+	{
+		a++;
+	}
+	return (a);
+}
+/**
  * new_dog - Creates a new dog instance using a struct
  * @name: name of the dog
  * @age: age of the dog
@@ -12,6 +28,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, j;
 	dog_t *copy;
 
 	copy = malloc(sizeof(dog_t));
@@ -19,22 +36,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	copy->name = malloc(strlen(name) + 1);
+	copy->name = malloc(_lenght(name) + 1);
 	if (copy->name == NULL)
 	{
 		free(copy);
 		return (NULL);
 	}
-	strcpy(copy->name, name);
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		copy->name[i] = name[i];
+	}
+	copy->name[i] = '\0';
 
-	copy->owner = malloc(strlen(owner) + 1);
+	copy->owner = malloc(_lenght(owner) + 1);
 	if (copy->owner == NULL)
 	{
 		free(copy->name);
 		free(copy);
 		return (NULL);
 	}
-	strcpy(copy->owner, owner);
+	for (j = 0; owner[j] != '\0'; j++)
+	{
+		copy->owner[j] = owner[j];
+	}
+	copy->owner[j] = '\0';
 
 	copy->age = age;
 
