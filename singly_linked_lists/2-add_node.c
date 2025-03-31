67761e4a434acdd9e44b17b24056104bcd33220a
Alexiscnl <alexis.cornillon@gmail.com>
2025-03-31 13:54:40 +0200

    add_betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+ * _lenght - Calcule la longueur d'une chaîne de caractères.
+ * @b: La chaîne de caractères dont la longueur doit être calculée.
  *
- *
- *
- *
+ * Return: La longueur de la chaîne.
  */
 int _lenght(char *b)
 {
@@ -19,10 +19,11 @@ int _lenght(char *b)
 	return (a);
 }
 /**
+ * add_node - Ajoute un nœud en tête de liste.
+ * @head: Pointeur vers le pointeur du premier élément de la liste.
+ * @str: La chaîne de caractères à ajouter dans le nouveau nœud.
  *
- *
- *
- *
+ * Return: L'adresse du nouveau nœud ou NULL en cas d'échec.
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -46,7 +47,7 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = copy;
 	new_node->len = _lenght(copy);
 	new_node->next = *head;
-	
+
 	*head = new_node;
 
 	return (new_node);
