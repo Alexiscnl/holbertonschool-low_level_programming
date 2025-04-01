1aebe1c6ab64c6f7da9dcb7050ba2e033b4b66c4
Alexiscnl <alexis.cornillon@gmail.com>
2025-04-01 09:29:18 +0200

    Function executed before main

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * attribute - Function executed before main
+ *
+ * This function is executed automatically before the main function
+ * due to the constructor attribute. It prints a message to the standard
+ * output.
+ */
+void attribute(void) __attribute__((constructor));
+void attribute(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon myback!\n");
+}
