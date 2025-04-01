beca6f160a930173b586e390ea25b957fff67a86
Alexiscnl <alexis.cornillon@gmail.com>
2025-04-01 14:48:31 +0200

    update betty

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -2,11 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
- /**
+/**
  * free_dlistint - Frees a doubly linked list
  * @head: The head of the list to be freed
  *
- * Description: This function traverses the list, freeing each node.
+ * Description: This function traverses the doubly linked list,
+ * freeing each node in the list. It starts from the head and
+ * moves through the list, freeing each node's memory until
+ * the end of the list is reached (when head is NULL).
  */
 void free_dlistint(dlistint_t *head)
 {
