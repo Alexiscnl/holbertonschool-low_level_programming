8703e45d84fe0709eae4a3fa340d19277300f80a
Alexiscnl <alexis.cornillon@gmail.com>
2025-04-01 10:19:33 +0200

    Prints all elements of a dlistint_t list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - Prints all elements of a dlistint_t list
+ * @h: Pointer to the first node of the list
+ *
+ * Return: The number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	for (; h !=  NULL; h = h->next)
+	{
+		printf("%u\n", h->n);
+		count++;
+	}
+	return (count);
+}
