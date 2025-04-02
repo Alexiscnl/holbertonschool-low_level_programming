c61c325f3d058727b4d143d79c7239266e7b25d9
Alexiscnl <alexis.cornillon@gmail.com>
2025-04-02 12:56:33 +0200

    add free temp

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -27,7 +27,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		{
 			(*head)->prev = NULL;
 		}
-
+		free(temp);
 		return (1);
 	}
 	while (temp && i < index)
@@ -43,5 +43,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (temp->next)
 		temp->next->prev = temp->prev;
 
+	free(temp);
 	return (1);
 }
