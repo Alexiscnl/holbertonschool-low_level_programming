8201b8f7b7a55e2d6bd46c0e7e6d170c667bc687
Alexiscnl <alexis.cornillon@gmail.com>
2025-04-04 12:03:57 +0200

    update copy_file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,13 +40,12 @@ int copy_file(const char *file_from, char *file_to)
 	ssize_t bytes_read, bytes_writen;
 
 	fd_from = open(file_from, O_RDONLY);
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
